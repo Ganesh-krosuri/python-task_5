1b04ea55432201774c5c0425d654f808a48b1617
Ganesh-krosuri <vtu29265@veltech.edu.in>
2025-10-29 19:43:21 +0530

    Update task_5(a).py

--- task_5(a).py
+++ task_5(a).py
@@ -1,21 +1,13 @@
-sample = []
-sample.insert(0, 12)
-print(sample)
-sample.insert(1, 4)
-print(sample)
-sample.insert(2, 2)
-print(sample)
-sample.insert(3, 4)
-print(sample)
-sample.remove(4)
-print(sample)
-sample.append(21)
-print(sample)
-sample.sort()
-print(sample)
-sample.pop()
-print(sample)
-sample.reverse()
-print(sample)
-a = list(range(10))
-print(a)
+def search(arr, x):
+    for i in range(len(arr)):
+        if arr[i] == x:
+            return i
+    return -1
+
+arr = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
+x = 110
+result = search(arr, x)
+if result != -1:
+    print("Element", x, "is present at index", result)
+else:
+    print("Element", x, "is not present in the array")
