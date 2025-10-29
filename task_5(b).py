693a145fd8fa29f017ea5ba39e3b24ace4ed668a
Ganesh-krosuri <vtu29265@veltech.edu.in>
2025-10-29 19:44:06 +0530

    Update task_5(b).py

--- task_5(b).py
+++ task_5(b).py
@@ -1,7 +1,16 @@
-alist = []
-for i in range(int(input())):
-    name = input()
-    score = float(input())
-    alist.append([name, score])
-second_highest = sorted(set([score for name, score in alist]))[1]
-print('\n'.join(sorted([name for name, score in alist if score == second_highest])))
+def binary_search(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+nums = [-1, 0, 3, 5, 9, 12]
+target = 9
+print(binary_search(nums, target))
